a40c5f5e54eb95e4d45bfa09be02e97ce02b3d82
Dmitry Ishchenko <19191458+DmitriyIshchenko@users.noreply.github.com>
2025-03-25 23:20:32 +0300

    refactor: remove useless code

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -54,9 +54,7 @@ function getPromiseResult(source) {
  * [Promise.reject(1), Promise.reject(2), Promise.reject(3)]    => Promise rejected
  */
 function getFirstResolvedPromiseResult(promises) {
-  return Promise.race(promises)
-    .then((value) => Promise.resolve(value))
-    .catch(() => Promise.reject());
+  return Promise.race(promises);
 }
 
 /**
